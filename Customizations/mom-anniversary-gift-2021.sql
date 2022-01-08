8b7233795fb7dd04b6d4124aebc5a4e9c26da588
Christopher Baker <christopher.ray.baker@gmail.com>
2022-01-08 08:20:07 -0600

    Add files via upload

---
+++ Customizations/mom-anniversary-gift-2021.sql
@@ -0,0 +1,3 @@
+DELETE from ace_world.quest where name='MomsAnniversary2021';
+INSERT INTO ace_world.quest (name, min_Delta, max_Solves, message) VALUES('MomsAnniversary2021', 0, 1, 'Gift to remember mom.');
+
