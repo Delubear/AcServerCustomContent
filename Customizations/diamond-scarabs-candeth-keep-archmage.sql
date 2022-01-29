c33742124921dc2a3f3af53ec787e216ff8a6b30
Christopher Baker <christopher.ray.baker@gmail.com>
2022-01-29 16:08:56 -0600

    Add files via upload

---
+++ Customizations/diamond-scarabs-candeth-keep-archmage.sql
@@ -0,0 +1,4 @@
+delete from 'weenie_properties_create_list' where 'object_Id' = 24594 and 'weenie_Class_Id' = 7299;
+
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (24594, 4,  7299,100, 0,    0, False) /* Diamond Scarab */;
