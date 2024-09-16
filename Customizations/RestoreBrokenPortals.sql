a1a0b6fb6c0b3581b9abf82bcb19b0841b2a5c87
Christopher Baker <christopher.ray.baker@gmail.com>
2024-09-16 13:53:33 -0500

    restore broken portals

---
+++ Customizations/RestoreBrokenPortals.sql
@@ -0,0 +1,189 @@
+/* Yanshi */
+UPDATE landblock_instance
+SET weenie_Class_Id = 8995
+WHERE weenie_Class_Id=1029;
+
+/* Sawato */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42849
+WHERE weenie_Class_Id=4043;
+
+/* Stonehold */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42811
+WHERE weenie_Class_Id=4044;
+
+/* Lin */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42844
+WHERE weenie_Class_Id=4567;
+
+/* Tou-Tou */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42841
+WHERE weenie_Class_Id=4568;
+
+/* Lytelthorpe */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42816
+WHERE weenie_Class_Id=4570;
+
+/* Al-Arqas */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42823
+WHERE weenie_Class_Id=4571;
+
+/* Khayyaban */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42822
+WHERE weenie_Class_Id=4572;
+
+/* Kryst */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42850
+WHERE weenie_Class_Id=8554;
+
+/* Ahurenga */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42827
+WHERE weenie_Class_Id=10982;
+
+/* Xarabydun */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42832
+WHERE weenie_Class_Id=26640;
+
+/* Eastwatch */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42839
+WHERE weenie_Class_Id=30383;
+
+/* Westwatch */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42837
+WHERE weenie_Class_Id=30384;
+
+/* Arwic */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42819
+WHERE weenie_Class_Id=1013;
+
+/* Cragstone */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42818
+WHERE weenie_Class_Id=1014;
+
+/* Eastham */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42815
+WHERE weenie_Class_Id=1016;
+
+/* Glenden Wood */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42814
+WHERE weenie_Class_Id=1017;
+
+/* Hebian-To */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42846
+WHERE weenie_Class_Id=1018;
+
+/* Holtburg */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42820
+WHERE weenie_Class_Id=1020;
+
+/* Mayoi */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42842
+WHERE weenie_Class_Id=1022;
+
+/* Qalaba'r */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42833
+WHERE weenie_Class_Id=1023;
+
+/* Rithwic */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42817
+WHERE weenie_Class_Id=1025;
+
+/* Samsur */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42834
+WHERE weenie_Class_Id=1026;
+
+/* Shoushi */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42840
+WHERE weenie_Class_Id=1027;
+
+/* Yaraq */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42824
+WHERE weenie_Class_Id=1030;
+
+/* Zaikhal */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42831
+WHERE weenie_Class_Id=1032;
+
+/* Uziz */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42821
+WHERE weenie_Class_Id=4036;
+
+/* Baishi */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42845
+WHERE weenie_Class_Id=4037;
+
+/* Tufa */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42829
+WHERE weenie_Class_Id=4038;
+
+/* Kara */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42848
+WHERE weenie_Class_Id=4041;
+
+/* Plateau */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42812
+WHERE weenie_Class_Id=4042;
+
+/* Nanto */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42843
+WHERE weenie_Class_Id=4569;
+
+/* Neydisa Castle */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42828
+WHERE weenie_Class_Id=6086;
+
+/* Bluespire */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42825
+WHERE weenie_Class_Id=11953;
+
+/* Greenspire */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42826
+WHERE weenie_Class_Id=11954;
+
+/* Redspire */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42836
+WHERE weenie_Class_Id=11960;
+
+/* Nanto #2? */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42843
+WHERE weenie_Class_Id=12160;
+
+/* Sanamar */
+UPDATE landblock_instance
+SET weenie_Class_Id = 42835
+WHERE weenie_Class_Id=28709;
